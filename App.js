d3caba1fde9756282410922572b2dd680ffd4a2c
Jack Moran <jvckmorvn@gmail.com>
2023-10-09 13:26:57 +0200

    init sqlite db

--- App.js
+++ App.js
@@ -1,16 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { Navigation, NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useEffect, useState } from 'react';
+import AppLoading from 'expo-app-loading';
 
 import AllPlaces from './screens/AllPlaces';
 import AddPlace from './screens/AddPlace';
 import Map from './screens/Map';
 import IconButton from './components/UI/IconButton';
 import { Colours } from './constants/colours';
+import { init } from './util/database';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [dbInitialised, setDbInitialised] = useState(false);
+
+  useEffect(() => {
+    init().then(() => {
+      setDbInitialised(true);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }, []);
+
+  if (!dbInitialised) {
+    return <AppLoading/>
+  }
+
   return (
     <>
       <StatusBar style='dark'/>
