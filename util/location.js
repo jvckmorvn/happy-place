52e58acde1edf5be5ea7e3e97d30c7673d46b456
Jack Moran <jvckmorvn@gmail.com>
2023-10-13 12:15:47 +0200

    hide api key

--- util/location.js
+++ util/location.js
@@ -1,12 +1,12 @@
-const GOOGLE_API_KEY = 'AIzaSyA6FXvc45zF-cKd47UW1aiygpK1otKatW4'
+import {GOOGLE_API_KEY} from '@env';
 
 export function getMapPreview(lat, long) {
-  const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${long}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${long}&key=${GOOGLE_API_KEY}`;
+  const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${long}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${long}&key=${process.env.GOOGLE_API_KEY}`;
   return imagePreviewUrl;
 }
 
 export async function getAddress(latitude, longitude) {
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${GOOGLE_API_KEY}`;
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${process.env.GOOGLE_API_KEY}`;
   const response = await fetch(url);
 
   if (!response.ok) {
